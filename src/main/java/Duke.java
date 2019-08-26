4de2aa7a88acb01a6e33056e012682896cd6644b
wallacelim97 <wallacelim97@gmail.com>
2019-08-27 01:12:07 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,33 +14,63 @@ public class Duke {
         System.out.println(lnBreak);
         System.out.println("What can I do for you?");
         System.out.println(lnBreak);
-
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
 
-        getInput: while(true) {
-            String input = sc.nextLine();
-            switch(input){
-                case "bye":
-                    System.out.println(lnBreak);
-                    System.out.println("    Bye. Hope to see you again soon!");
-                    System.out.println(lnBreak);
-                    break getInput;
-                case "list":
-                    System.out.println(lnBreak);
-                    printList(list);
-                    System.out.println(lnBreak);
-                    break;
-                default:
-                    list.add(input);
-                    System.out.println(lnBreak);
-                    System.out.println("    added: " + input);
-                    System.out.println(lnBreak);
+        getInput:
+        while (true) {
+            String input = sc.next();
+            switch (input) {
+            case "bye":
+                System.out.println(lnBreak);
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(lnBreak);
+                break getInput;
+            case "list":
+                System.out.println(lnBreak);
+                printList(list);
+                System.out.println(lnBreak);
+                break;
+            case "done":
+                int idx = sc.nextInt() - 1;
+                sc.nextLine();
+                list.get(idx).setDone();
+                System.out.println(lnBreak);
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("    " + list.get(idx).toString());
+                System.out.println(lnBreak);
+            case "todo":
+                String todo = sc.nextLine();
+                Task td = new Task('T', todo);
+                list.add(td);
+                System.out.println(lnBreak);
+                System.out.println("    Got it. I've added this task:\n    " + td);
+                System.out.println("    You now have " + list.size() + " tasks.");
+                System.out.println(lnBreak);
+                break;
+            case "deadline":
+                String[] dl = sc.nextLine().split("/");
+                Task deadline = new Task('D', dl[0], dl[1]);
+                list.add(deadline);
+                System.out.println(lnBreak);
+                System.out.println("    Got it. I've added this deadline:\n    " + deadline);
+                System.out.println("    You now have " + list.size() + " tasks.");
+                System.out.println(lnBreak);
+                break;
+            case "event":
+                String[] ev = sc.nextLine().split("/");
+                Task event = new Task('E', ev[0], ev[1]);
+                list.add(event);
+                System.out.println(lnBreak);
+                System.out.println("    Got it. I've added this event:\n    " + event);
+                System.out.println("    You now have " + list.size() + " tasks.");
+                System.out.println(lnBreak);
+                break;
             }
         }
     }
 
-    static void printList(ArrayList list) {
+    private static void printList(ArrayList list) {
         for (int i = 1; i <= list.size(); i++) {
             System.out.printf("    %d. %s\n", i, list.get(i-1));
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+    private String details;
+    private char type;
+    private boolean isDone;
+
+    public Task(char type, String description) {
+        this.description = description;
+        this.type = type;
+        this.isDone = false;
+    }
+
+    public Task(char type, String description, String details) {
+        this.description = description;
+        this.type = type;
+        this.details = details;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "O" : "X"); //return tick or X symbols
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public String getDesc () {
+        return this.description;
+    }
+
+    public String toString() {
+        if (this.details == null) {
+            return String.format("[%c][%s] %s", this.type, this.getStatusIcon(), this.description);
+        } else {
+            return String.format("[%c][%s]%s%s", this.type, this.getStatusIcon(), this.description, this.details);
+        }
+    }
 }
