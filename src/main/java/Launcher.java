8716356f009cae2c8671d7a66a297ef456280573
wallacelim97 <wallacelim97@gmail.com>
2019-09-03 17:27:46 +0800

    Added workable JavaFX GUI

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,11 @@
-package duke;
-
 import javafx.application.Application;
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        System.out.println("Launching");
+        Application.launch(Main.class, args);
+        System.out.println("Launched");
     }
 }
